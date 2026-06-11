bc5711017e6731c5faff3151125dbce87969bc93
Juram <gimjulam2@gmail.com>
2026-06-11 11:42:06 +0900

    Add Profile Page

--- src/pages/Profile/components/ProfileHeader.jsx
+++ src/pages/Profile/components/ProfileHeader.jsx
@@ -10,6 +10,8 @@ export default function Profile() {
         setFollowing(!isFollowing);
     }
 
+    const favGroup = ["BTS","BOYNEXTDOOR","BIGBANG"];
+
     return (
         <>
         <div className="ProfileCard">
@@ -17,23 +19,34 @@ export default function Profile() {
                 <div className="ProfileImage">
                     <img src={ExampleProfileImage} alt="ProfileImage" />
                 </div>
-                <div className="userIntroduction">
-                    <p className="userName">USERNAME</p>
+                <div className="userInfo">
+                    <div className="userIntro">
+                        <p className="userName">USERNAME</p>
+                        <p className="introduction">소개글</p>
+                    </div>
                     <div className="following-Follwer">
                         <div className="following">
-                            <p>0 팔로잉</p>
+                            <p>0</p><p className="followNum">팔로잉</p>
                         </div>
                         <div className="follower">      
-                            <p>0 팔로워</p>
+                            <p>0</p><p className="followNum">팔로워</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div>
-                <button onClick={toggleFollow} className="follow-btn">
-                    {isFollowing ? '팔로잉' : '팔로우'}
-                </button>
-                <button><img src={kebabIcon} alt=""/></button>
+            <div className="userSet">
+                <div className="userBtn">
+                    <button 
+                        onClick={toggleFollow}
+                        className={`follow-btn ${isFollowing ? 'active' : ''}`}
+                    >
+                        {isFollowing ? '팔로잉' : '팔로우'}
+                    </button>
+                    <button className="drop-btn"><img src={kebabIcon}/></button>
+                </div>
+                <div className="userFavGroups">
+                    {/* 좋아하는 그룹있으면 추가 */}
+                </div>
             </div>
         </div>
         </>
