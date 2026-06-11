b067bb8a2bd400ab450f03edea6c0842c39f6085
Juram <gimjulam2@gmail.com>
2026-06-12 03:39:55 +0900

    userFavGroups, favGroups 더미 데이터 생성

--- src/pages/Profile/components/ProfileHeader.jsx
+++ src/pages/Profile/components/ProfileHeader.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import "./ProfileHeader.css";
-import "../../../index.css"
+import "../../../index.css";
 import ExampleProfileImage from "../../../assets/example-profile.svg";
-import kebabIcon from "../../../assets/kebab-icon.svg"
+import kebabIcon from "../../../assets/kebab-icon.svg";
+import BtsIcon from "../../BTS-Logo.png";
+
 export default function Profile() {
     const [isFollowing, setFollowing] = useState(false);
 
+    const [favGroups, setFavGroups] = useState([
+        {id: 1, name: "BTS", logo: BtsIcon},
+        {id: 2, name: "BOYNEXTDOOR", logo: BtsIcon},
+        {id: 3, name: "BIGBANG", logo: BtsIcon},
+        {id: 4, name: "NCT", logo: BtsIcon}
+        
+    ]);
+
     const toggleFollow = () => {
         setFollowing(!isFollowing);
-    }
+    };
 
-    const favGroup = ["BTS","BOYNEXTDOOR","BIGBANG"];
+    const handleAddGroup = () => {
+        // 최애 그룹 추가 페이지로 이동
+    };
 
     return (
         <>
@@ -24,16 +36,19 @@ export default function Profile() {
                         <p className="userName">USERNAME</p>
                         <p className="introduction">소개글</p>
                     </div>
-                    <div className="following-Follwer">
-                        <div className="following">
-                            <p>0</p><p className="followNum">팔로잉</p>
+                    <div className="following-Follower">
+                        <div className="follow-stat">
+                            <p className="followCount">0</p>
+                            <p className="followLabel">팔로잉</p>
                         </div>
-                        <div className="follower">      
-                            <p>0</p><p className="followNum">팔로워</p>
+                        <div className="follow-stat">      
+                            <p className="followCount">0</p>
+                            <p className="followLabel">팔로워</p>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div className="userSet">
                 <div className="userBtn">
                     <button 
@@ -42,10 +57,25 @@ export default function Profile() {
                     >
                         {isFollowing ? '팔로잉' : '팔로우'}
                     </button>
-                    <button className="drop-btn"><img src={kebabIcon}/></button>
+                    <button className="drop-btn">
+                        <img src={kebabIcon} alt="더보기"/>
+                    </button>
                 </div>
+
                 <div className="userFavGroups">
-                    {/* 좋아하는 그룹있으면 추가 */}
+                    {favGroups.length > 0 ? (
+                        <div className="groupBadgeList">
+                            {favGroups.map((group) => (
+                                <div key={group.id} className="groupBadge" title={group.name}>
+                                    <img src={group.logo} alt={group.name} />
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <button className="add-group-btn" onClick={handleAddGroup}>
+                            + 최애 그룹 추가하기
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
