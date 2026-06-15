e2a442cbecf6621b1d7495636cf390b2a574a41c
Juram <gimjulam2@gmail.com>
2026-06-16 01:02:38 +0900

    IdolNews 와 Reoute 연결

--- src/pages/Home/components/NewsContainer.jsx
+++ src/pages/Home/components/NewsContainer.jsx
@@ -1,16 +1,40 @@
+import { Link } from "react-router-dom";
 import NewsIcon from "../../../assets/news-icon.svg";
 import NewsCard from "./NewsCard.jsx";
-export default function NewsContainer(){
-    return(
-        <div className="news-container">
-            <div className="news-header">
-                <img src={NewsIcon} alt="news-icon" />
-                <p className="title">실시간 뉴스</p>
-            </div>
-            <div className="news-box">
-                <button>모두보기</button>
-                <NewsCard/>
-            </div>
+
+export default function NewsContainer() {
+
+    return (
+    <div className="news-container">
+        <div style={styles.newsHeader}>
+            <img src={NewsIcon} alt="news-icon" />
+            <p className="title">실시간 뉴스</p>
         </div>
-    )
+        <div style={styles.newsBox}>
+            <Link to="/idol-news" style={styles.newsBoxButton}>
+                    모두보기
+            </Link>
+            <NewsCard />
+        </div>
+    </div>
+    );
 }
+
+const styles = {
+    newsHeader: {
+        display: "flex",
+        gap: "15px",
+        alignItems: "center",
+    },
+    newsBox: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "5px",
+    },
+    newsBoxButton: {
+        color: "var(--gray-2)",
+        textDecoration : "none",
+        fontSize: "20px",
+        marginLeft: "auto",
+    },
+};
