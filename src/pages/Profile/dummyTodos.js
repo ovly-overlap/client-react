1f1ba6fc10c05d39d6ac6abc72dc4bb5cc8cda56
Juram <gimjulam2@gmail.com>
2026-06-16 01:01:31 +0900

    dummyTodos에 date 추가

--- src/pages/Profile/dummyTodos.js
+++ src/pages/Profile/dummyTodos.js
@@ -1,6 +1,6 @@
 export const dummyTodos = [
-    { id: 1, label: '디자인 시안 검토하기', done: false },
-    { id: 2, label: '주간 보고서 작성', done: true },
-    { id: 3, label: '팀 미팅 준비', done: false },
-    { id: 4, label: '코드 리뷰 완료', done: false },
+    { id: 1, label: '지민 생카가기', memo: null, date: 2026-6-15, done: false },
+    { id: 2, label: '보넥도 앨범 사기', memo: null, date: 2026-6-15, done: true },
+    { id: 3, label: '보넥도 자컨 보기', memo: null, date: 2026-6-15, done: false },
+    { id: 4, label: 'BTS 오레오 사기', memo: null, date: 2026-6-15, done: false },
 ];
