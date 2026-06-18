ecc0a4ba8aa992ce05968a7c0bd0c98f1229cdfd
Juram <gimjulam2@gmail.com>
2026-06-18 10:10:10 +0900

    feat(css): signuplogo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,24 +31,25 @@ function Login() {
         <div className='upupup'>
           <div className="upupupMention">
             <img src={logo} alt='ovlylogo' className='LOGO'/>
-            <span className='title'>ovly</span>
+            <div className='title'>ovly</div> 
           </div>
           <p className='subtitle'>최애 아티스트와 함께하는 특별한 공간</p>
         </div>
-        <br />
-        <div>
-          <label className='ididid' htmlFor="uaserid">아이디</label><br />
-          <input type="text" id='userid2' name='userid2' required placeholder='아이디를 입력해주세요.' value={userid} onChange={(e) => setUserid(e.target.value)} />
-        </div>
-        <br />
-        <div>
-          <label className='ididid' htmlFor="password">비밀번호</label><br />
-          <input type="password" id='password2' name='password2' required placeholder='비밀번호를 입력해주세요.' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <div className="idPassSection">
+          <div className="idSection">
+            <label className='ididid' html96For="uaserid">아이디</label>
+            <input type="text" id='userid2' name='userid2' required placeholder='아이디를 입력해주세요.' value={userid} onChange={(e) => setUserid(e.target.value)} />
+          </div>
+          <div className="passSection">
+            <label className='ididid' htmlFor="password">비밀번호</label>
+            <input type="password" id='password2' name='password2' required placeholder='비밀번호를 입력해주세요.' value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
         </div>
         {error && <p className="errorMsg">{error}</p>}
-        <br/>
-        <button className='start' type='submit'>로그인</button>
-        <p>아직 계정이 없으신가요? <Link to="/signup">회원가입</Link></p>
+        <div className="loginButton">
+          <button className='start' type='submit'>로그인</button>
+          <p>아직 계정이 없으신가요? <Link to="/signup">회원가입</Link></p>
+        </div>
         </form>
         </div>
       </div>
